d5e0061d2d533f3a74bd324fee01e016a1a4a431
Bhavin Pathak <bhavin.rndtechnosoft@gmail.com>
2023-11-03 16:51:12 +0530

    Initial Commit

---
+++ Explorer SwiftUi with SWIFT 5.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Explorer SwiftUi with SWIFT 5.0
+//
+//  Created by Ravi Shah on 03/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Explorer SwiftUi with SWIFT 5.0/Explorer_SwiftUi_with_SWIFT_5_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Explorer_SwiftUi_with_SWIFT_5_0App.swift
+//  Explorer SwiftUi with SWIFT 5.0
+//
+//  Created by Ravi Shah on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Explorer_SwiftUi_with_SWIFT_5_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
